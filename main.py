2cb7102a8c0c82f66569c6eea58f6b18e0810d71
Alan Ngo <u3556436@connect.hku.hk>
2020-07-05 19:25:52 +0800

    Adding menu

--- main.py
+++ main.py
@@ -10,7 +10,19 @@ class Application(tk.Frame):
     def main(self):
         pass
     def menu(self):
-        pass
+        # Menubar
+        menubar = tk.Menu(self.master)
+        self.master.config(menu=menubar)
+
+        #creating file menu
+        filemenu = tk.Menu(menubar)
+        filemenu.add_command(label="Exit", command=self.exit)
+        #creating help menu
+        helpmenu = tk.Menu(menubar)
+        helpmenu.add_command(label="Help")
+
+        menubar.add_cascade(label="File", menu=filemenu)
+        menubar.add_cascade(label="Help", menu=helpmenu)
     def other(self):
         self.title = tk.Label(self)
         self.title["text"] = "Math Quiz"
@@ -21,20 +33,12 @@ class Application(tk.Frame):
         self.credit["text"] = "credit by William & Alan"
         self.credit.pack(side="bottom")
         self.credit.config(font=("credit by William & Alan", 12))
+    def exit(self):
+        exit()
 root = tk.Tk()
 app = Application(master=root)
 root.title("Math QUIZ")
 root.geometry("500x500")
-#Menubar
-menubar = tk.Menu(root)
-filemenu = tk.Menu(menubar, tearoff = 0)
-filemenu.add_command(label = "Exit", command = root.quit)
-
-helpmenu = tk.Menu(menubar, tearoff = 0)
-helpmenu.add_command(label = "Help")
-
-menubar.add_cascade(label = "File", menu = filemenu)
-menubar.add_cascade(label = "Help", menu = helpmenu)
 
 app.mainloop()
 
